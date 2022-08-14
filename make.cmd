b19a8583d1dd6cfddbc2bd98b5e8eb25fad54dc7
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-08-14 20:10:16 +0200

    Minimal set of files to enable installation

---
+++ make.cmd
@@ -0,0 +1,42 @@
+@echo off
+if "%1" == "all" (
+    call %0 install
+    call %0 check
+    goto :eof
+)
+if "%1" == "check" (
+    echo ***
+    echo *** Running test suite
+    echo ***
+    cd tests
+    python -m unittest discover -s . -p *.py -v
+    goto :eof
+)
+if "%1" == "clean" (
+    echo ***
+    echo *** Cleaning files
+    echo ***
+    for /D /r %%i in (__pycache*__) do rmdir /S /Q "%%i"
+    for /D %%i in (build dist iq.egg-info) do if exist "%%i" rmdir /S /Q "%%i"
+    goto :eof
+)
+if "%1" == "install" (
+    call %0 clean
+    call %0 reinstall
+    goto :eof
+)
+if "%1" == "reinstall" (
+    echo ***
+    echo *** Installing library
+    echo ***
+    pip uninstall -y iq
+    pip install .
+    goto :eof
+)
+echo Unknown option %1
+exit -1
+if "%1" == "docs" (
+    cd docs
+    call %0 html
+    cd ..
+)
