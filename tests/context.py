f00550cae70bd57ad5a65c75ee5915314fb01fab
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-10-09 18:07:33 +0200

    Enable running api-public tests against AWS server

--- tests/context.py
+++ tests/context.py
@@ -1,14 +1,30 @@
 import inspirationq.api.bare
+import inspirationq.api.aws
 import unittest
+import os
 
-common_credentials = inspirationq.api.bare.bare_credentials("http://127.0.0.1:3000/")
 
-import sys
-import os.path
+def make_default_credentials(url=None):
+    if url is None:
+        url = os.getenv("IQ_URL")
+    if url is None or url == "local":
+        return inspirationq.api.bare.bare_credentials("http://127.0.0.1:3000/")
+    if url == "aws":
+        url = None
+    username = os.getenv("IQ_USERNAME")
+    password = os.getenv("IQ_PASSWORD")
+    if username is None:
+        raise Exception(
+            "Remote testing requested but environment variable IQ_USERNAME not set"
+        )
+    if password is None:
+        raise Exception(
+            "Remote testing requested but environment variable IQ_PASSWORD not set"
+        )
+    return inspirationq.api.aws.iQAWSAPI(username, password, url)
 
-for core_location in ["../../api/core/tests", "../api/core/tests"]:
-    if os.path.exists(core_location):
-        sys.path += os.path.realpath(core_location)
+
+common_credentials = make_default_credentials()
 
 
 class BaseAPITest(unittest.TestCase):
