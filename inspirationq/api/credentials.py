9753ff0fee0c13a36b05ae9949d3d488b8d4e151
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-10-09 17:29:06 +0200

    Fixed the list of available API entry points

--- inspirationq/api/credentials.py
+++ inspirationq/api/credentials.py
@@ -1,52 +1,63 @@
 import requests
 
 known_entry_points = {
-    'hello',
-    'qubo_bf',
-    'qubo_cont',
-    'qubo_mc'
+    "hello",
+    "qubo_bf",
+    "qubo_cont",
+    "qubo_mc",
+    "qubo_sa",
+    "qbo_ca",
+    "tsp_mc",
+    "qudo_mc",
 }
 
-class BaseAPI:
 
+class BaseAPI:
     def __init__(self):
         self.url_dict = {}
         self.debug = False
 
     def authorization(self):
-        raise Exception(f'Unauthorized access to Inspiration-Q API. Please obtain credentials first')
+        raise Exception(
+            f"Unauthorized access to Inspiration-Q API. Please obtain credentials first"
+        )
 
     def specialize(self, base_url, entry_points):
-        return { k : base_url + '/' + k for k in entry_points }
+        return {k: base_url + "/" + k for k in entry_points}
 
     def get(self, function, **arguments):
         headers = self.authorization()
         if function not in self.url_dict:
-            raise Exception(f'Unknown API function {function}')
+            raise Exception(f"Unknown API function {function}")
         url = self.url_dict[function]
         if self.debug:
-            print(f'Sending GET request to {url} with header:\n{headers}\n'
-                  f'and arguments:\n{arguments}')
+            print(
+                f"Sending GET request to {url} with header:\n{headers}\n"
+                f"and arguments:\n{arguments}"
+            )
         r = requests.get(url=url, headers=headers, **arguments)
         if self.debug:
-            print(f'Received request Response {r} with content:\n{r.content}')
+            print(f"Received request Response {r} with content:\n{r.content}")
         if not r.ok:
-            raise Exception(f'Error returned from Inspiration-Q API: {r}')
+            raise Exception(f"Error returned from Inspiration-Q API: {r}")
         return r.json()
 
     def post(self, function, **arguments):
         headers = self.authorization()
         if function not in self.url_dict:
-            raise Exception(f'Unknown API function {function}')
+            raise Exception(f"Unknown API function {function}")
         url = self.url_dict[function]
         if self.debug:
-            print(f'Sending POST request to {url} with header:\n{headers}\n'
-                  f'and arguments:\n{arguments}')
+            print(
+                f"Sending POST request to {url} with header:\n{headers}\n"
+                f"and arguments:\n{arguments}"
+            )
         r = requests.post(url=url, headers=headers, **arguments)
         if self.debug:
-            print(f'Received request Response {r} with content:\n{r.content}')
+            print(f"Received request Response {r} with content:\n{r.content}")
         if not r.ok:
-            raise Exception(f'Error returned from Inspiration-Q API: {r}')
+            raise Exception(f"Error returned from Inspiration-Q API: {r}")
         return r.json()
+
 
 default_credentials = BaseAPI()
